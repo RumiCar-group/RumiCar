3889efa42081eda88c223591111ea0d4f9a80064
z10000 <49936800+z10000@users.noreply.github.com>
2020-05-10 16:13:47 +0900

    Update Exercise-2.2.py

--- RasPi/Exercise-2.2/Exercise-2.2.py
+++ RasPi/Exercise-2.2/Exercise-2.2.py
@@ -1,1 +1,16 @@
+import RumiCar
+import time
+
+if __name__ == "__main__":
+    while True:
+        try:
+            RumiCar.rc_drive(RumiCar.FORWARD, 900000)
             time.sleep(3)
+            RumiCar.rc_drive(RumiCar.FORWARD, 700000)
+            time.sleep(3)
+            RumiCar.rc_drive(RumiCar.FORWARD, 500000)
+            time.sleep(3)
+            RumiCar.rc_drive(RumiCar.FORWARD, 300000)
+            time.sleep(3)
+        except KeyboardInterrupt:
+            RumiCar.rc_clear()
