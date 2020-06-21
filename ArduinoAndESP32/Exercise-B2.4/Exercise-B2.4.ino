df798b44c1fe1143c2432211e9a57d8f9033feb2
z10000 <49936800+z10000@users.noreply.github.com>
2020-06-21 17:30:20 +0900

    Update Exercise-B2.4.ino

--- ArduinoAndESP32/Exercise-B2.4/Exercise-B2.4.ino
+++ ArduinoAndESP32/Exercise-B2.4/Exercise-B2.4.ino
@@ -1,12 +1,3 @@
-/* This example shows how to get single-shot range
-  measurements from the VL53L0X. The sensor can optionally be
-  configured with different ranging profiles, as described in
-  the VL53L0X API user manual, to get better performance for
-  a certain application. This code is based on the four
-  "SingleRanging" examples in the VL53L0X API.
-
-  The range readings are in units of mm. */
-
 #include <Wire.h>
 
 #include <VL53L0X.h>
